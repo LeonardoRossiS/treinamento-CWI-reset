fcd7ab41ead93dd9a09c14152daf709e933a85d8
Leonardo de Rossi <leoderossi@gmail.com>
2021-10-14 19:21:05 -0300

    Commit 2

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,18 +1,35 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utils.Browser;
 import utils.Utils;
-
 import static org.junit.Assert.assertTrue;
 
 public class SetupTest extends BaseTest{
-
     @Test
     public void testOpeningBrowserAndLoadingPage(){
         assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseURL()));
         System.out.println("Abrimos o navegador e carregamos a URL");
     }
+    @Test
+    public void testLogIn(){
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl()
+                .contains(Utils.getLoginURL()));
+        System.out.println("Acessamos a página de login");
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys(Utils.getLoginEmail());
+        System.out.println("Preenchemos o e-mail");
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys(Utils.getLoginPassword());
+        System.out.println("Preenchemos a senha");
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+        System.out.println("Clicamos em Sign In");
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl()
+                .contains(Utils.getMyAccountURL()));
+        System.out.println("Acessamos a página do usuário");
+        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading"))
+                .getText().contains("MY ACCOUNT"));
+        System.out.println("Confirmamos o heading da página do usuário");
 
-
+    }
 }
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -5,4 +5,13 @@ public class Utils {
     public static String getBaseURL(){
         return "http://automationpractice.com/";
     }
+    public static String getLoginURL(){
+        return getBaseURL().concat("index.php?controller=authentication&back=my-account");
+    }
+    public static String getMyAccountURL(){
+        return getBaseURL().concat("index.php?controller=my-account");
+    }
+    public static String getLoginEmail(){ return "testeLeonardo@CWI.br";}
+    public static String getLoginPassword(){ return "abc123";}
+
 }
